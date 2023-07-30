f4fff70ce5cd4c9b58573ade41487cbbefde0ed1
RT <rustytrim11@gmail.com>
2023-07-30 15:00:08 -0400

    Uses cdn express server now

--- src/routes/guild.ts
+++ src/routes/guild.ts
@@ -1,10 +1,8 @@
 import express from "express"
 import { verifyToken } from "../middleware/Verification";
 import Validator from "../utils/Validator";
-import Collection from "../utils/Collection";
-import { uploadFile } from "../utils/FileUtils";
 import Guild from "../models/Guild";
-import Generator from "../utils/Generator";
+import Collection from "../utils/Collection";
 
 const router = express.Router();
 
@@ -14,9 +12,33 @@ router.post("/", (req, res) => {
     Validator.validateGuildCreate().validate(req.body).then(() => {
         const guild = new Guild({ name: req.body.name, ownerId: (req as any).user.id });
 
+        // if (req.body.icon) {
+        //     uploadFile(`${Buffer.from(guild._id).toString("base64url")}`, req.body.icon).then(() => {
+        //         guild.icon = `${Generator.base64UrlEncode(guild._id)}.png`;
+        //         guild.save().then(() => {
+        //             Collection.channels.create({ name: "General", type: 0, guildId: guild._id, position: 0 });
+        //             Collection.channels.create({ name: "General", type: 1, guildId: guild._id, members: [], position: 1 });
+        //         });
+        //         (req as any).user.guilds.push(guild._id);
+        //         (req as any).user.save().then(() => {
+        //             res.status(200).json({ guild });
+        //         });
+        //     });
+        //     return;
+        // }
+
         if (req.body.icon) {
-            uploadFile(`images/guild/${Generator.base64UrlEncode(guild._id)}.png`, req.body.icon).then(() => {
-                guild.icon = `http://localhost:3001/cdn/images/guild/${Generator.base64UrlEncode(guild._id)}.png`
+            fetch(`http://localhost:3002/guild/${Buffer.from(guild._id).toString("base64url")}.png`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `${req.get("Authorization")}`
+                },
+                body: JSON.stringify({
+                    data: req.body.icon.split(",")[1]
+                })
+            }).then(() => {
+                guild.icon = `${Buffer.from(guild._id).toString("base64url")}.png`;
                 guild.save().then(() => {
                     Collection.channels.create({ name: "General", type: 0, guildId: guild._id, position: 0 });
                     Collection.channels.create({ name: "General", type: 1, guildId: guild._id, members: [], position: 1 });
@@ -25,7 +47,7 @@ router.post("/", (req, res) => {
                 (req as any).user.save().then(() => {
                     res.status(200).json({ guild });
                 });
-            });
+            })
             return;
         }
 
